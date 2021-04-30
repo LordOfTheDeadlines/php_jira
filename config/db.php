a2a12b4de7512a064d1789ccb54e4d9176622628
Anastasia Sidorchuk <vedroid28@mail.ru>
2021-04-30 10:02:37 +0500

    make table_user_migration

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'onlus',
+    'password' => '28udarov',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
